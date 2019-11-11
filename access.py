69b3be8581405576391c3c758084c5240814f0b3
Lakshman Chinta <mithrachinta@gmail.com>
2019-11-11 14:48:33 -0500

    Access Done
    Accessing jira and creating an issue

--- access.py
+++ access.py
@@ -1,28 +1,17 @@
-# This script shows how to use the client in anonymous mode
-# against jira.atlassian.com.
 from jira import JIRA
-import re
-
-# By default, the client will connect to a JIRA instance started from the Atlassian Plugin SDK
-# (see https://developer.atlassian.com/display/DOCS/Installing+the+Atlassian+Plugin+SDK for details).
-# Override this with the options parameter.
 options = {
     'server': '	https://axelaar.atlassian.net'}
 user = 'mithrachinta@gmail.com'
-apikey = 'LT7IB6kDf1tVNYrUFflW38D8'
 apikey2 = 'nL5tOLsoihlfa29ExsVe3A85'
 pwd= 'Axelaar@4'
-
-oauth_dict = {
-    'access_token': 'foo',
-    'access_token_secret': 'bar',
-    'consumer_key': 'jira-oauth-consumer',
-    'key_cert': 'test'
-}
 jira = JIRA(basic_auth=(user,apikey2),options=options)
-#jira = JIRA(options)
+def create_issue():
+    jira.create_issue(summary="Third Issue", description="Lakshman Chinta", issuetype={'name': 'Task'}, project='JF',
+                              assignee={'name': 'lakshmanchinta'})
 
 
-# Get all projects viewable by anonymous users.
+
+create_issue()
 projects = jira.projects()
 print(projects)
+
